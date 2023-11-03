e17f4ab03549e7e0ad0116c3b5e8ac163819424f
ImShaune <nndesigner@outlook.es>
2023-11-03 13:02:18 -0600

    navbar section

---
+++ src/components/Navigation.tsx
@@ -0,0 +1,49 @@
+"use client";
+
+import { navMenuList } from "@/data/navigationData";
+import { CustomLink } from "./CustomLink";
+import { FiMenu } from 'react-icons/fi'
+import { cn } from '@/utils/helper'
+import {useState} from 'react';
+import {AiOutlineClose} from 'react-icons/ai'
+
+export function Navigation() {
+
+    const [isNavOpen, setNavOpen] = useState(false) 
+
+    return (
+        <nav className="py-4 border-b border-zinc-800 shadow-md sticky top-0 z-10 bg-zinc-900/20 backdrop-blur-md">
+            <div className="max-w-7xl mx-auto px-4 md:px-8 flex justify-between items-center">
+            <p>Logo?</p>
+            <div className="md:hidden" onClick={() => setNavOpen((prevState) => !prevState)}>
+            {isNavOpen ? ( 
+                <AiOutlineClose className="text-2xl" /> 
+                ) : ( 
+                <FiMenu className="text-2xl"/> )}
+                
+            </div>
+            <div className={cn(
+                'max-md:fixed max-md:top-[58px] max-md:py-10 max-md:left-0 max-md:w-full max-md:min-h-screen max-md:bg-zinc-900/90 max-md:backdrop-blur-lg',
+                !isNavOpen && 'max-md:hidden'
+                )}
+                >
+                <ul className="flex items-center flex-col md:flex-row gap-6">
+                    {
+                    navMenuList.map(({ title, href }, index) => {
+                        const isLastElement = index === navMenuList.length -1;
+                        return <li key={href}>
+                            <CustomLink 
+                            href={href} 
+                            linkType={isLastElement ? 'primary' : 'link'}
+                            >
+                                {title}
+                                
+                            </CustomLink>
+                        </li>
+                    })}
+                </ul>
+            </div>
+            </div>
+        </nav>
+    )
+}
---
+++ src/data/navigationData.ts
@@ -0,0 +1,26 @@
+export const navMenuList = [
+    {
+        title: 'Home',
+        href: '#home'
+    },
+
+    {
+        title: 'About Me',
+        href: '#about-me'
+    },
+
+    {
+        title: 'Skills',
+        href: '#skills'
+    },
+
+    {
+        title: 'Projects',
+        href: '#projects'
+    },
+
+    {
+        title: 'Hire Me',
+        href: 'mailto:nahuel.dev99@gmail.com'
+    }
+]
