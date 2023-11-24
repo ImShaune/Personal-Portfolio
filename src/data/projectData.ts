81b51bcbcadb38895592707179e2946411db36f9
ImShaune <nndesigner@outlook.es>
2023-11-23 23:27:08 -0600

    data projects and project added

---
+++ src/data/projectData.ts
@@ -0,0 +1,58 @@
+export const projects = [
+    {
+        title: "Crypto App Prices", 
+        desc: "Frontend web application developed in Vue.js to create the interface allowing users to track the cryptocurrency market in real time. The application is connected to an API to display the data on screen.",
+        stack: ["VueJs"],
+        image: "/app1.png",
+        sourceCode: "https://github.com/ImShaune/crypto-app-prices",
+        demo: "https://crypto-app-prices.vercel.app/",
+    },
+    
+    {
+        title: " ", 
+        desc: " ",
+        stack: [" "],
+        image: "",
+        sourceCode: "",
+        demo: "",
+    },
+
+    {
+        title: " ", 
+        desc: " ",
+        stack: [" "],
+        image: "",
+        sourceCode: "",
+        demo: "",
+    },
+
+    {
+        title: " ", 
+        desc: " ",
+        stack: [" "],
+        image: "",
+        sourceCode: "",
+        demo: "",
+    },
+
+    {
+        title: " ", 
+        desc: " ",
+        stack: [" "],
+        image: "",
+        sourceCode: "",
+        demo: "",
+    },
+
+    {
+        title: " ", 
+        desc: " ",
+        stack: [" "],
+        image: "",
+        sourceCode: "",
+        demo: "",
+    },
+
+    
+
+]
